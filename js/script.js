271bc6d94f6f72ce13a8fed929ba1222cf91dec3
SlyBeard <StudyBeard@gmail.com>
2022-10-27 14:15:34 +0300

    Added cicles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+'use strict';
+
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
 
 const personalMovieDB = {
     count: numberOfFilms, //сюда передается ответ на первый вопрос
@@ -9,12 +12,29 @@ const personalMovieDB = {
 
 }
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+}  else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+
+for (let i=0; i<2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
+
+    if (a !=null && b != null && a !='' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
+
